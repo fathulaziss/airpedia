9c319d93fbf0a91b97c36b85510a27bcc336286c
Muhamad Fathul Azis <azisfathl@gmail.com>
2022-12-02 21:41:37 +0700

    add data destination

---
+++ lib/app/data/destination_data.dart
@@ -0,0 +1,87 @@
+const List newDestination = [
+  {
+    'about':
+        'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+    'airport_destination_code': 'amq',
+    'airport_destination_location': 'Ambon',
+    'airport_destination_name': 'Pattimura',
+    'country': 'Indonesia',
+    'destination_name': 'Ambon',
+    'image': 'img_new_destination4.png',
+    'interests': ['Kids Park', 'Honor Bridge', 'City Musseum', 'Central Mall'],
+    'photos': [
+      'img_destination_detail1.png',
+      'img_destination_detail2.png',
+      'img_destination_detail3.png',
+    ],
+    'rating': 4.5,
+  },
+  {
+    'about':
+        'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+    'airport_destination_code': 'djj',
+    'airport_destination_location': 'Jayapura',
+    'airport_destination_name': 'Sentani',
+    'country': 'Indonesia',
+    'destination_name': 'Raja Ampat',
+    'image': 'img_new_destination3.png',
+    'interests': ['Kids Park', 'Honor Bridge', 'City Musseum', 'Central Mall'],
+    'photos': [
+      'img_destination_detail1.png',
+      'img_destination_detail2.png',
+      'img_destination_detail3.png',
+    ],
+    'rating': 4.8,
+  },
+  {
+    'about':
+        'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+    'airport_destination_code': 'dps',
+    'airport_destination_location': 'Denpasar',
+    'airport_destination_name': 'Ngurah Rai',
+    'country': 'Indonesia',
+    'destination_name': 'Bali',
+    'image': 'img_new_destination5.png',
+    'interests': ['Kids Park', 'Honor Bridge', 'City Musseum', 'Central Mall'],
+    'photos': [
+      'img_destination_detail1.png',
+      'img_destination_detail2.png',
+      'img_destination_detail3.png',
+    ],
+    'rating': 4.7,
+  },
+  {
+    'about':
+        'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+    'airport_destination_code': 'pdg',
+    'airport_destination_location': 'Padang',
+    'airport_destination_name': 'Minangkabau',
+    'country': 'Indonesia',
+    'destination_name': 'Padang',
+    'image': 'img_new_destination2.png',
+    'interests': ['Kids Park', 'Honor Bridge', 'City Musseum', 'Central Mall'],
+    'photos': [
+      'img_destination_detail1.png',
+      'img_destination_detail2.png',
+      'img_destination_detail3.png',
+    ],
+    'rating': 4.7,
+  },
+  {
+    'about':
+        'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+    'airport_destination_code': 'upg',
+    'airport_destination_location': 'Makassar',
+    'airport_destination_name': 'Sultan Hasanuddin',
+    'country': 'Indonesia',
+    'destination_name': 'Makassar',
+    'image': 'img_new_destination1.png',
+    'interests': ['Kids Park', 'Honor Bridge', 'City Musseum', 'Central Mall'],
+    'photos': [
+      'img_destination_detail1.png',
+      'img_destination_detail2.png',
+      'img_destination_detail3.png',
+    ],
+    'rating': 4.5,
+  },
+];
---
+++ lib/app/models/destination_model.dart
@@ -0,0 +1,68 @@
+import 'package:equatable/equatable.dart';
+
+class DestinationModel extends Equatable {
+  const DestinationModel({
+    this.about = '',
+    this.airportDestinationCode = '',
+    this.airportDestinationLocation = '',
+    this.airportDestinationName = '',
+    this.country = '',
+    this.destinationName = '',
+    this.image = '',
+    this.interests = const [],
+    this.photos = const [],
+    this.rating = 0,
+  });
+
+  factory DestinationModel.fromJson(Map<String, dynamic> json) =>
+      DestinationModel(
+        about: json['about'],
+        airportDestinationCode: json['airport_destination_code'],
+        airportDestinationLocation: json['airport_destination_location'],
+        airportDestinationName: json['airport_destination_name'],
+        country: json['country'],
+        destinationName: json['destination_name'],
+        image: json['image'],
+        interests: json['interests'],
+        photos: json['photos'],
+        rating: json['rating'],
+      );
+
+  final String about;
+  final String airportDestinationCode;
+  final String airportDestinationLocation;
+  final String airportDestinationName;
+  final String country;
+  final String destinationName;
+  final String image;
+  final List<String> interests;
+  final List<String> photos;
+  final double rating;
+
+  Map<String, dynamic> toJson() => {
+        'about': about,
+        'airport_destination_code': airportDestinationCode,
+        'airport_destination_location': airportDestinationLocation,
+        'airport_destination_name': airportDestinationName,
+        'country': country,
+        'destination_name': destinationName,
+        'image': image,
+        'interests': interests,
+        'photos': photos,
+        'rating': rating,
+      };
+
+  @override
+  List<Object?> get props => [
+        about,
+        airportDestinationCode,
+        airportDestinationLocation,
+        airportDestinationName,
+        country,
+        destinationName,
+        image,
+        interests,
+        photos,
+        rating,
+      ];
+}
