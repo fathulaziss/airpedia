6bb8b4b28591fe53a19df2d0391184c1490ff86e
Muhamad Fathul Azis <azisfathl@gmail.com>
2022-12-02 22:05:25 +0700

    update data destination

--- lib/app/data/destination_data.dart
+++ lib/app/data/destination_data.dart
@@ -1,3 +1,91 @@
+const List recommendedDestination = [
+  {
+    'about':
+        'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+    'airport_destination_code': 'lop',
+    'airport_destination_location': 'Lombok Tengah',
+    'airport_destination_name': 'Zainuddin Abdul Madjid',
+    'country': 'Indonesia',
+    'destination_name': 'Lombok',
+    'image': 'img_destination4.png',
+    'interests': ['Kids Park', 'Honor Bridge', 'City Musseum', 'Central Mall'],
+    'photos': [
+      'img_destination_detail1.png',
+      'img_destination_detail2.png',
+      'img_destination_detail3.png',
+    ],
+    'rating': 5.0,
+  },
+  {
+    'about':
+        'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+    'airport_destination_code': 'bdo',
+    'airport_destination_location': 'Bandung',
+    'airport_destination_name': 'Husein Sastranegara',
+    'country': 'Indonesia',
+    'destination_name': 'Bandung',
+    'image': 'img_destination1.png',
+    'interests': ['Kids Park', 'Honor Bridge', 'City Musseum', 'Central Mall'],
+    'photos': [
+      'img_destination_detail1.png',
+      'img_destination_detail2.png',
+      'img_destination_detail3.png',
+    ],
+    'rating': 4.8,
+  },
+  {
+    'about':
+        'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+    'airport_destination_code': 'jog',
+    'airport_destination_location': 'Yogyakarta',
+    'airport_destination_name': 'Adi Sucipto',
+    'country': 'Indonesia',
+    'destination_name': 'Yogyakarta',
+    'image': 'img_destination2.png',
+    'interests': ['Kids Park', 'Honor Bridge', 'City Musseum', 'Central Mall'],
+    'photos': [
+      'img_destination_detail1.png',
+      'img_destination_detail2.png',
+      'img_destination_detail3.png',
+    ],
+    'rating': 4.7,
+  },
+  {
+    'about':
+        'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+    'airport_destination_code': 'sub',
+    'airport_destination_location': 'Surabaya',
+    'airport_destination_name': 'Juanda',
+    'country': 'Indonesia',
+    'destination_name': 'Surabaya',
+    'image': 'img_destination5.png',
+    'interests': ['Kids Park', 'Honor Bridge', 'City Musseum', 'Central Mall'],
+    'photos': [
+      'img_destination_detail1.png',
+      'img_destination_detail2.png',
+      'img_destination_detail3.png',
+    ],
+    'rating': 4.7,
+  },
+  {
+    'about':
+        'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+    'airport_destination_code': 'cgk',
+    'airport_destination_location': 'Jakarta',
+    'airport_destination_name': 'Soekarno Hatta',
+    'country': 'Indonesia',
+    'destination_name': 'Jakarta',
+    'image': 'img_destination3.png',
+    'interests': ['Kids Park', 'Honor Bridge', 'City Musseum', 'Central Mall'],
+    'photos': [
+      'img_destination_detail1.png',
+      'img_destination_detail2.png',
+      'img_destination_detail3.png',
+    ],
+    'rating': 4.8,
+  },
+];
+
 const List newDestination = [
   {
     'about':
